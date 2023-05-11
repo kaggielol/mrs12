3476a61b8a97c79577688334593d497be4465dbd
kaggielol <99984771+kaggielol@users.noreply.github.com>
2023-05-11 17:11:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 11 09:35:50 2023
+
+@author: CMP
+"""
+
+from flask import Flask,session,render_template,request,redirect,url_for
+
+app=Flask(__name__)
+
+app.secret_key='asdsdfsdfs13sdf_df%&'
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        session['username']=request.form['username']
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username',None)
+    return redirect(url_for('index'))
+
+@app.route('/')
+def index():
+    login=False
+    if 'username' in session:
+        login=True
+    return render_template('login_home.html',login=login)
+
+if __name__=='__main__':
+    app.run(debug=False)
